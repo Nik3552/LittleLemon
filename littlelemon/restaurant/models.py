d4b54b9dd984f7c6e8f2c11777d774366df3626e
Mykyta <kolan5031@gmail.com>
2025-03-14 10:38:06 +0100

    Added booking and menu models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Booking(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateField()
+
+    class Meta:
+        db_table = "Booking"
+
+    def __str__(self):
+        return self.Name
+
+
+class Menu(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
+
+    class Meta:
+        db_table = "Menu"
+
+    def __str__(self):
+        return self.Title
